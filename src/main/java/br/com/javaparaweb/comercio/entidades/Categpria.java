8bb4d0a5607e1ab62518715aa1ea00df4a0e368f
evandrosuzart <evandrossuzart@outlook.com>
2017-12-13 17:34:49 -0200

    criada classe categoria

---
+++ src/main/java/br/com/javaparaweb/comercio/entidades/Categpria.java
@@ -0,0 +1,13 @@
+package br.com.javaparaweb.comercio.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
+public class Categpria implements Serializable {
+	
+
+}
